577d48ff0cbc1ea545118ef5b3d4f8883f8bd6c0
Adam Ryman <adamryman@gmail.com>
2016-01-15 19:28:49 -0800

    Append &raw=1 to make dropbox links direct

--- .scrotbox.sh
+++ .scrotbox.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 scrot -s "$HOME/dropbox/Dropbox/devbook/upload/%Y-%m-%dT%T%z.png" -e '/home/adamryman/bin/dropbox sharelink $f | xsel -i -b'
+paste -d '\0' <(xsel -b) <(echo "&raw=1") | xsel -i -b
